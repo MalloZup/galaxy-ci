81e3598864d00add699635b6148a618345b95b53
Dario Maiocchi <dmaiocchi@suse.com>
2017-01-17 16:53:54 +0100

    Fix

--- git/list-prs.rb
+++ git/list-prs.rb
@@ -8,23 +8,26 @@ require 'json'
 # https://developer.github.com/v3/pulls/#list-pull-requests
 
 
-#  create a ~/.netrc file, like this:
-# machine api.github.com
-#   login MYBOT_USER
-#  password MYPASSWORD
-
 # is only on of some login methods
 client = Octokit::Client.new(:netrc => true)
+$repo = 'SUSE/spacewalk'
 
-client.auto_paginate = true
-
-prs = client.pull_requests('MalloZup/spacewalk', :state => 'open')
+prs = client.pull_requests($repo, :state => 'open')
 prs.each do |pr|
         puts(pr.number)
         puts(pr.head.sha)
         # branch
         puts(pr.base.ref)
-        puts(pr.state)
         puts(pr.title)
+        puts(pr.head.title)
         puts(pr.body)
+        # get status
+        pr_state = client.status($repo, pr.head.sha)
+        puts "STATUS OF PR IS:  " + pr_state.state
+        puts
+        puts "****************************************************************************"
+        puts "****************************************************************************"
+        #  pending, success, error, or failure.
+        #status = client.create_status($repo, "14ccc24fd574eafac203a3dc8d77da790c6321fa","failure")
+      #  status = client.create_status($repo, pr.head.sha, "pending")
 end
