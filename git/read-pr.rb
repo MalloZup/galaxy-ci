11028f1540fae4556264c2a9c3f8166554f3e7de
Dario Maiocchi <MalloZup@users.noreply.github.com>
2017-01-23 11:08:20 +0100

    delete file

--- git/read-pr.rb
+++
@@ -1,9 +0,0 @@
-#/usr/bin/ruby
-
-require 'json'
-conf = JSON.parse(File.read('PR.json'))
-puts conf['repo']
-puts conf['context']
-puts conf['description']
-puts conf['target_url']
-puts conf['git_dir']
