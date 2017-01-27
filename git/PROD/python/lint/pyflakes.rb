00cabaf539f1ca5670af3a20c3d05198db43eeae
Dario Maiocchi <dmaiocchi@suse.com>
2017-01-27 17:44:40 +0100

    better output

--- git/PROD/python/lint/pyflakes.rb
+++ git/PROD/python/lint/pyflakes.rb
@@ -1,12 +1,8 @@
 #!/usr/bin/ruby
-require 'octokit'
 
-## INPUT:
+require 'octokit'
 # repo to fetch
 repo = 'SUSE/spacewalk'
-# context is the label printed by the bot into github, in the checks
-target_url = 'https://ci.suse.de/view/Manager/view/Manager-Head/job/' \
-             "manager-Head-test-github-pr/#{ENV['JOB_NUMBER']}"
 # git_dir is where we have the github repo in our machine
 $git_dir = '/tmp/spacewalk/'
 $python_files = []
@@ -14,7 +10,6 @@ context = "python-tests"
 description = "pyflakes-checkstyle"
 $client = Octokit::Client.new(netrc: true)
 $j_status = 'success'
-
 def pyflakes_test(upstream, pr, repo)
   # get author:
   pr_com = $client.commit(repo, pr)
@@ -35,10 +30,10 @@ def pyflakes_test(upstream, pr, repo)
    end
   `git checkout #{upstream}`
   `git branch -D  PR-#{pr}`
-   output.each do | out | 
+   output.each do | out |
      $comment << out
    end
-   $comment << "great job, no pyflakes failures" if $j_status == 'success'
+   $comment << "great job, no pyflakes failures\n" if $j_status == 'success'
    $comment << " ```"
 end
 
@@ -49,6 +44,7 @@ def check_for_files(repo, pr, type)
   end
 end
 
+# we put the results on the comment.
 def create_comment(repo, pr, comment)
   $client.create_commit_comment(repo, pr, comment)
 end
@@ -74,25 +70,23 @@ prs.each do |pr|
       $client.create_status(repo, pr.head.sha, $j_status,  context: context, description: description)
       # create comment
       create_comment(repo, pr.head.sha, $comment)
-      break 
+      break
     end
   end
   puts '******************************'
   puts 'PR is already reviewed by bot'
   puts '******************************'
-  if pr_state.statuses[0]['description'] != description || pr_state.statuses[0]['state'] == 'pending' || 
-     pr_state.statuses[0]['state'] == 'failure'
-     check_for_files(repo, pr.head.sha, '.py')
-     next if $python_files.any? == false
-     $client.create_status(repo, pr.head.sha, 'pending', context: context, description: description)
-     pyflakes_test(pr.base.ref, pr.head.sha, repo)
-     $client.create_status(repo, pr.head.sha, $j_status,
-                         context: context, description: description)
-     create_comment(repo, pr.head.sha, $comment)
-     break 
+  if pr_state.statuses[0]['description'] != description ||
+     pr_state.statuses[0]['state'] == 'pending' 
+    check_for_files(repo, pr.head.sha, '.py')
+    next if $python_files.any? == false
+    $client.create_status(repo, pr.head.sha, 'pending', context: context, description: description)
+    pyflakes_test(pr.base.ref, pr.head.sha, repo)
+    $client.create_status(repo, pr.head.sha, $j_status, context: context, description: description)
+    create_comment(repo, pr.head.sha, $comment)
+    break
   end
-  next if pr_state.statuses[0]['description']  == description
+  next if pr_state.statuses[0]['description'] == description
 end
-
-# jenkins 
+# jenkins
 exit 1 if $j_status == 'failure'
