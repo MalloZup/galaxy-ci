a0b0f5999e83398bc16f0776bd09f255dccbbba4
dario <darioszr@gmail.com>
2017-03-19 15:28:31 +0100

    improve barchart

--- immagine/cocomero/datavisualize.rb
+++ immagine/cocomero/datavisualize.rb
@@ -6,27 +6,25 @@ require 'json'
 # Doc here:
 # http://www.rubydoc.info/github/topfunky/gruff/Gruff/Bezier
 
-def bezier
-  g = Gruff::Bezier.new
-  g.title = "Failures Spacewalk-testsuite"
-  # read number of failures from json file.
-  # each entry corrispond a run , so the results
-  # this can be like for years or something.
-  g.data 'test-failures', [0,500, 32, 93, 39,44,44,44,1, 48, 88, 90, 100]
-  g.x_axis_label = 'test runs'
-  g.y_axis_label = 'failures'
-  g.write("results.png")
+class Failures_show
+  def initialize()
+    @failures = []
+  end
+  def drawbar(output)
+    g = Gruff::Bar.new
+    g.title = 'Failaures of Spacewalk-testsuite'
+    # todo, puts maybe the date as label tagged
+    # g.labels = { 0 => '5/6', 1 => '5/15', 2 => '5/24', 3 => '5/30', 4 => '6/4'}
+    g.data('Failures', @failures[1])
+    g.write(output)
+  end
+  # read array in json
+  def get_results(file_json)
+    json = File.read(file_json)
+    @failures= JSON.parse(json)
+  end
 end
 
-def xy_line
-  # xy_line is more weekly report
-  g = Gruff::Line.new
-  g.title = 'Results Spacewalk-testsuite-base'
-  g.dataxy('failures', [1, 2, 3, 4, 5], [1, 3, 4, 5, 6])
-  # label should be expanded per each day.
-  g.labels = {0 => 'Montag', 2 => 'Dienstag', 3 => 'Mittwoch', 4 => 'Donnerstag', 5 => 'Freitag'}
-  g.write('line_xy.png')
-end
-
-bezier
-xy_line
+bezier = Failures_show.new
+bezier.get_results('results.json')
+bezier.drawbar("results.png")
